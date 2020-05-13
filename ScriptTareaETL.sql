4ce6ef2b93dd412737cafa6b5b6ff9f49c9b8d6d
leoba11 <leobarri.2013@gmail.com>
2020-05-13 11:54:42 -0600

    Script de base de datos y agregar componente limpieza en proyecto

---
+++ ScriptTareaETL.sql
@@ -0,0 +1,108 @@
+USE BaseETLTarea2
+
+CREATE TABLE Employee
+(
+	EmployeeKey			INT PRIMARY KEY,
+	NationalIDNumber	NVARCHAR(15),
+	FirstName			NVARCHAR(50),
+	MiddleName			NVARCHAR(50),
+	LastName			NVARCHAR(50),
+	MaritalStatus		NCHAR(1),
+	Salaried			BIT,
+	Gender				NCHAR(1)
+)
+
+CREATE TABLE Territory
+(
+	TerritoryKey	 			INT PRIMARY KEY,
+	ProvinceStateName			NVARCHAR(50),
+	ContryName					NVARCHAR(50)
+)
+
+CREATE TABLE Date
+(
+	DateKey			INT			 PRIMARY KEY,
+	Date			DATE		 NOT NULL,
+	DayOfWeek		NVARCHAR(10) NOT NULL,
+	FiscalYear		INT			 NOT NULL,	
+	FiscalQuarter	INT,
+	Event			VARCHAR(50)	 NOT NULL,		
+	Season			VARCHAR(15)	 NOT NULL,
+	Month			VARCHAR(15)	 NOT NULL,
+	Quarter			INT			 NOT NULL,
+	Year			INT			 NOT NULL,
+	WeekdayFlag		BIT			 NOT NULL
+)
+
+
+CREATE TABLE Product
+(
+	ProductKey				INT	NOT NULL,
+	Name					NVARCHAR(50),	
+	Size					NVARCHAR(5),
+	Weight					DECIMAL(8,2),
+	ListPrice				MONEY,
+	StandarCost				MONEY,
+	Color					NVARCHAR(15),
+	Class					NCHAR(2),
+	Style					NCHAR(2),
+	Line					NCHAR(2),
+	Category				NVARCHAR(50),
+	Subcategory				NVARCHAR(50),
+	Model					NVARCHAR(50),
+	PRIMARY KEY(Productkey)
+)
+
+
+CREATE TABLE FactSalesPerfomance
+(	
+	FactSalesPerfomanceKey 	INT PRIMARY KEY,
+	Quantity				INT,
+	Price					INT,
+	Amount					INT,
+ 	Employee_FKey			INT,
+	Territory_FKey			INT,
+	Date_FKey				INT,
+	Product_FKey			INT,
+ 	CONSTRAINT FK_Fact_Employee			FOREIGN KEY (Employee_FKey)			REFERENCES Employee(EmployeeKey),
+	CONSTRAINT FK_Fact_Territory		FOREIGN KEY (Territory_FKey)		REFERENCES Territory(TerritoryKey),
+	CONSTRAINT FK_Fact_Date				FOREIGN KEY (Date_FKey)				REFERENCES Date(DateKey),
+	CONSTRAINT FK_Fact_Product			FOREIGN KEY (Product_FKey)			REFERENCES Product(ProductKey)
+); 
+
+
+--Datos de prueba 
+
+
+INSERT INTO Employee(EmployeeKey, NationalIDNumber)
+VALUES(0, '901090640')
+
+INSERT INTO Date
+VALUES
+
+INSERT INTO Product(ProductKey, Name, Color)
+VALUES(0, 'Coca', 'rojo')
+
+INSERT INTO Territory(TerritoryKey, ProvinceStateName, ContryName)
+VALUES(0, 'Alajuela', 'CR')
+
+INSERT INTO FactSalesPerfomance(FactSalesPerformanceKey, Employee_FKey, Territory_FKey, Date_FKey, Product_FKey)
+VALUES(0, 0, 0, 0, 0)
+
+
+--SELECT PRUEBAS
+
+SELECT *
+FROM Date
+
+SELECT *
+FROM Product
+
+SELECT *
+FROM Territory
+
+SELECT *
+FROM Employee
+
+SELECT *
+FROM FactSalesPerfomance
